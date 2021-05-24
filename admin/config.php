045f3ff7fd7c4009705a1f36c150eeaa0f0f067d
ealpha072 <ealpha072@gmail.com>
2021-05-24 20:58:37 +0300

    Login function added

--- admin/config.php
+++ admin/config.php
@@ -12,4 +12,41 @@
         //throw $th;
         //echo "Connection failed".$e->getMessage();
     }
+    //prepare statments
+    $sql1 = $conn->prepare("SELECT * FROM admin WHERE username =? AND password=?");
+    
+    if(isset($_POST['login']) && $_SERVER["REQUEST_METHOD"]=="POST"){
+        login();
+    }
+
+    //login code
+    function login(){
+        global $sql1;
+        $username = $_POST["username"];
+        $password = $_POST["password"];
+        $sql1->execute(array($username, $password));
+        //$rows = $sql1->rowCount();
+        $results = $sql1->fetchAll(PDO::FETCH_ASSOC);
+        echo var_dump($results);
+        if( $username===$results[0]['username'] && $password===$results[0]['password']){
+            //set session variables
+            $_SESSION['loggedin']=true;
+			$_SESSION['username'] = $results[0]['username'];
+            $_SESSION['msg'] = "You are logged in as an admin";
+
+            header("location: dashboard.php");
+        }else{
+            echo "There was an error logging you in, check the credentials and try again";
+        }
+    }
+    
+
+    
+        
+
+
+
+
+    
+
 ?>
