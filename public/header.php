19719a133555dd6535b156c47f0bd5a6b8f9dc70
ealpha072 <ealpha072@gmail.com>
2021-05-24 19:19:58 +0300

    Footer and header added

---
+++ public/header.php
@@ -0,0 +1,12 @@
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv = "x-ua-compatible" content="ie=edge"/>
+  <title>Alpha Library</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ <!--<div class="d-flex container-fluid">-->
