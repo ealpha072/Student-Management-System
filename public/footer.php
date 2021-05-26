8be3ddd112e063f225d109e34db1a94adbc5c235
ealpha072 <ealpha072@gmail.com>
2021-05-26 11:42:53 +0300

    Js code for toggling menu

--- public/footer.php
+++ public/footer.php
@@ -6,6 +6,14 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    
+    <script>
+        $("#menu-toggle").click(function(e) {
+            e.preventDefault();
+            $("#wrapper").toggleClass("toggled");
+        });
+        $("input.sb").focus(function(e) {
+            $('.result-holder').html('').fadeOut(2000);
+        })
+    </script>   
 </body>
 </html>
