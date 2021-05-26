656ec3fc40f8f8b383061fc52258b5363d22706e
ealpha072 <ealpha072@gmail.com>
2021-05-26 20:03:09 +0300

    Replaced forms with require statements

--- admin/viewschool.php
+++ admin/viewschool.php
@@ -12,27 +12,7 @@
             <h5>View Units</h5>
         </div>
         <div class="card-body">
-            <div class="form">
-                <div class="limit" style="float: left;">
-                    <form action="" class="form-inline">
-                        <div class="form-group">
-                            <label for="inputPassword6">Show
-                                <input type="number" class="form-control mx-sm-3" min="1" max="<?php echo count($results)?>">entries
-                            </label>
-                        </div>
-                    </form>
-                </div>
-                
-                <div class="search" style="float:right">
-                    <form class="form-inline" action="">
-                        <div class="form-group">
-                            <label for="inputPassword6">Search</label>
-                            <input type="text" id="search" class="form-control mx-sm-3">
-                        </div>
-                    </form>
-                </div>                
-            </div>
-
+            <?php require "../public/forms.php"?>
             <div class="table">
                 <table class="table table-striped table-bordered">
                     <thead>
---
+++ public/forms.php
@@ -0,0 +1,20 @@
+<div class="form">
+                <div class="limit" style="float: left;">
+                    <form action="" class="form-inline">
+                        <div class="form-group">
+                            <label for="inputPassword6">Show
+                                <input type="number" class="form-control mx-sm-3" min="1" max="<?php echo count($results)?>">entries
+                            </label>
+                        </div>
+                    </form>
+                </div>
+                
+                <div class="search" style="float:right">
+                    <form class="form-inline" action="">
+                        <div class="form-group">
+                            <label for="inputPassword6">Search</label>
+                            <input type="text" id="search" class="form-control mx-sm-3">
+                        </div>
+                    </form>
+                </div>                
+</div>
