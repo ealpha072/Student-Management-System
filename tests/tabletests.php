a83d70d623f26147d8cca03837b3b9e035dc85d8
ealpha072 <ealpha072@gmail.com>
2021-05-29 14:17:59 +0300

    SQL query to group by course name

--- tests/tabletests.php
+++ tests/tabletests.php
@@ -1,47 +1,31 @@
 <?php require "../admin/config.php";
-    $array = [];
+    $schoolsarray = [];
+
     $arr = [];
 
     $mysql = $conn ->prepare("SELECT DISTINCT school FROM courses");
-    $mysql1 = $conn->prepare("SELECT full_name FROM courses where school=?");
+    $mysql1 = $conn->prepare("SELECT short_name, full_name FROM courses where school=?");
+
     //$mysql2 = $conn->prepare("SELECT school, full_name FROM courses WHERE school IN (SELECT DISTINCT school from courses)");
     $mysql->execute();
-    //$mysql2->execute();
-    $results = $mysql->fetchAll(\PDO::FETCH_ASSOC);
-
-    //$res = $mysql2->fetchAll(PDO::FETCH_ASSOC);
-    //print_r($res)."<br>";
+    $results = $mysql->fetchAll(PDO::FETCH_ASSOC);
 
 
-
-    array_push($array );
-
-    foreach ($results as $sch) {
-        # code...
-        array_push($array, $sch["school"]);
+    foreach ($results as $school) {
+        array_push($schoolsarray, $school["school"]);
     }
 
     
 
-    foreach ($array as $row) {
+    foreach ($schoolsarray as $row) {
         $mysql1->execute(array($row));
 
         $re = $mysql1->fetchAll(PDO::FETCH_ASSOC);
         array_push($arr, $re);
         
         //echo var_dump($re)."<br>";
+        print_r($re);
 
 
     }
-
-    foreach ($arr as $key) {
-        # code...
-        //echo var_dump($key)."<br>";
-        foreach ($key as $k) {
-            # code...
-            echo $k["full_name"]."<br>";
-        }
-    }
-    
-   // echo var_dump($arr["full_name"]);
 ?>
