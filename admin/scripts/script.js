c9ff5a5d38713f3c02e2d6497c52b02020ee897f
ealpha072 <ealpha072@gmail.com>
2021-06-10 18:31:36 +0300

    Restored from deleted files

---
+++ admin/scripts/script.js
@@ -0,0 +1,25 @@
+function showCourse() {
+    var myDiv = document.getElementById("coursetable");
+
+    //console.log(schoolOption.childNodes)
+    var request;
+    try {
+        request = new XMLHttpRequest;
+
+    } catch (error) {
+        alert("Ypur browser doesnt support AJAX!!")
+    }
+
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            myDiv.innerHTML = "";
+            myDiv.innerHTML = request.responseText;
+        }
+    }
+
+    var schoolOption = document.getElementById("schooloption").value;
+    var query = "?schoolname=" + schoolOption;
+    request.open("GET", "../jquery/processcourse.php" + query, true);
+    request.send(null);
+
+}
