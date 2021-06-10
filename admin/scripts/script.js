a41ad360a5a6e1a600ed5fa2e1c1f95e08b4da61
ealpha072 <ealpha072@gmail.com>
2021-06-10 14:58:52 +0300

    Open and send ajax request with a query string

--- admin/scripts/script.js
+++ admin/scripts/script.js
@@ -1,6 +1,7 @@
 function showCourse() {
     var myDiv = document.getElementById("coursetable");
-    console.log(myDiv.childNodes)
+
+    console.log(schoolOption.childNodes)
     var request;
     try {
         request = new XMLHttpRequest;
@@ -11,8 +12,14 @@ function showCourse() {
 
     request.onreadystatechange = function() {
         if (request.readyState === 4 && request.status === 200) {
-
+            myDiv.innerHTML = "";
+            myDiv.innerHTML = request.responseText;
         }
     }
+
+    var schoolOption = document.getElementById("schooloption").value;
+    var query = "?schoolname=" + schoolOption;
+    request.open("GET", "../jquery/processcourse.php" + query, true);
+    request.send(null);
 
 }
