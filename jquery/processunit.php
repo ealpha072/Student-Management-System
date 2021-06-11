a5c5e6fc055c90ab2cf9774db851fc3be58f7769
ealpha072 <ealpha072@gmail.com>
2021-06-11 15:29:51 +0300

    Fixed indentation in file

--- jquery/processunit.php
+++ jquery/processunit.php
@@ -1,17 +1,17 @@
 <?php
-require "../admin/config.php";
-$school = $_GET["school"];
-$sql = $conn->prepare("SELECT name FROM departments WHERE school = ?");
-$sql->execute(array($school));
-$results = $sql->fetchAll(PDO::FETCH_ASSOC);
-//value=\"". $row["name"]."\"
+    require "../admin/config.php";
+    $school = $_GET["school"];
+    $sql = $conn->prepare("SELECT name FROM departments WHERE school = ?");
+    $sql->execute(array($school));
+    $results = $sql->fetchAll(PDO::FETCH_ASSOC);
+    //value=\"". $row["name"]."\"
 
-echo "<select name=\"dpts\" class=\"form-control form-control\">";
-    echo "<option value=\"\" selected disabled>Choose a Department.........</option> ";
-    foreach($results as $row){
-        echo "<option value=\"".$row["name"]."\">"; 
-        echo $row["name"]; 
-        echo "</option>"; 
-    }
-echo "</select>";
+    echo "<select name=\"dpts\" class=\"form-control form-control\">";
+        echo "<option value=\"\" selected disabled>Choose a Department.........</option> ";
+        foreach($results as $row){
+            echo "<option value=\"".$row["name"]."\">"; 
+            echo $row["name"]; 
+            echo "</option>"; 
+        }
+    echo "</select>";
 ?>
