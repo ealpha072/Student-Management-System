aa77554e548ff5a867f7b225fbb44c0cc948d99b
ealpha072 <ealpha072@gmail.com>
2021-06-11 15:25:54 +0300

    All necessary code for adding new units

---
+++ admin/scripts/unitprocess.js
@@ -0,0 +1,21 @@
+function showUnit() {
+    var dpt = document.getElementById("dptselect");
+    var request;
+
+    try {
+        request = new XMLHttpRequest;
+    } catch (error) {
+        alert("Your browser doesnt support ajax requests");
+    }
+    request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 200) {
+            dpt.innerHTML = "";
+            dpt.innerHTML = request.responseText;
+        }
+    }
+    var school = document.getElementById("schoolselect").value;
+    var query = "?school=" + school;
+    request.open("GET", "../jquery/processunit.php" + query, true);
+    request.send(null)
+
+}
