6f8fa2de9a1db372ac8ada7bc35392d492f03f24
ealpha072 <ealpha072@gmail.com>
2021-06-21 20:37:50 +0300

    Added card class to div body to hold contents

--- admin/managecourses.php
+++ admin/managecourses.php
@@ -1,4 +1,26 @@
 <?php
     require "dashboard.php";
     require "config.php";
+
+    $sql3->execute();
+    $results = $sql3->fetchAll(PDO::FETCH_ASSOC);
 ?>
+<div class="container">
+    <div class="card">
+        <div class="card-header">
+            <h3>Manage courses</h3>
+        </div>
+        <div class="card-body">
+            <div class="form">                    
+                <div class=" search" style="float:left">
+                    <select name="school" id="schooloption" class="form-control" onchange="showCourse()">
+                        <option value="" selected disabled>View by school</option>
+                        <?php foreach($schools as $school){?>
+                        <option value="<?php echo $school["school_name"];?>"><?php echo $school["school_name"];?></option>
+                        <?php }?>
+                    </select>
+                </div>                
+            </div>
+        </div>
+    </div>
+</div>
